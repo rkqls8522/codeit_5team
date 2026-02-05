9d4151f7a34678dbf1f5e45013534bc7c35c8768
nnbok12 <nnbok12@gmail.com>
2026-02-05 10:57:02 +0900

    fix: transform_module.py 보완 및 transforms 옵션 추가

--- src/data_engineer/transform_module.py
+++ src/data_engineer/transform_module.py
@@ -1,20 +1,23 @@
 from torchvision.transforms import v2
+import torch
 
                                         ###     padding과 Resize가 작용된 transform 입니다.
 train_transforms = v2.Compose([
-        v2.Pad(
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Pad(
         padding=(152,0),
         fill=255,
         padding_mode="constant"
     ),
     v2.RandomRotation(360,fill=255),
-    #v2.RandomPosterize(bits=6,p=1.0),
     v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
     v2.Resize((600,600)),
 ])
-
 test_transforms = v2.Compose([
-        v2.Pad(
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Pad(
         padding=(152,0),
         fill=255,
         padding_mode="constant"
@@ -22,17 +25,99 @@ test_transforms = v2.Compose([
     v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
     v2.Resize((600,600)),
 ])
+v1_transform = (train_transforms, test_transforms)
 
 
-                                        ###     padding과 Resize가 적용되지 않은 transform 입니다. 원본 사이즈를 입력 해야할 시 사용.
-no_Resize_train_transforms = v2.Compose([
+train_transforms2 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Pad(
+        padding=(152,0),
+        fill=255,
+        padding_mode="constant"
+    ),
     v2.RandomRotation(360,fill=255),
-    #v2.RandomPosterize(bits=6,p=1.0),
-    v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
+    v2.Resize((600,600)),
 ])
+test_transforms2 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Pad(
+        padding=(152,0),
+        fill=255,
+        padding_mode="constant"
+    ),
+    v2.Resize((600,600)),
+])
+v2_transform = (train_transforms2, test_transforms2)
 
-no_Resize_test_transforms = v2.Compose([
+
+train_transforms3 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Pad(
+        padding=(152,0),
+        fill=255,
+        padding_mode="constant"
+    ),
+    v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
+    v2.Resize((600,600)),
+])
+test_transforms3 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Pad(
+        padding=(152,0),
+        fill=255,
+        padding_mode="constant"
+    ),
+    v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
+    v2.Resize((600,600)),
+])
+v3_transform = (train_transforms3, test_transforms3)
+
+
+
+###---------------- 리사이즈, padding 하지 않음
+
+
+
+n_train_transforms = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.RandomRotation(360,fill=255),
     v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
 ])
+n_test_transforms = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
+])
+n_v1_transform = (n_train_transforms, n_test_transforms)
+
+
+n_train_transforms2 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.RandomRotation(360,fill=255),
+])
+n_test_transforms2 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+])
+n_v2_transform = (n_train_transforms2, n_test_transforms2)
+
+
+n_train_transforms3 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
+])
+n_test_transforms3 = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.RandomAdjustSharpness(sharpness_factor=100.0, p=1.0),
+])
+n_v3_transform = (n_train_transforms3, n_test_transforms3)
 
 
